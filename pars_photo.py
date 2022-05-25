1a995b44ee337326d7952d7558524347be848cf9
slaukan <slavaboiko1997@gmail.com>
2022-05-25 18:19:21 +0300

    переделал через рекурсию

--- pars_photo.py
+++ pars_photo.py
@@ -5,23 +5,28 @@ import time
 
 start_time = datetime.now()
 
-tek_dir = os.getcwd() # Текущая деректория:
-print(tek_dir)
-dir = 'фото_1'        # папка откуда копируем фото
-dir2 = 'фото_2'       # папка куда копируем
+PATH_PHONE = r"C:\Users\HP\python\агрегатор\photo_phone"
+PATH_PC = r"C:\Users\HP\python\агрегатор\photo_PC"
 
-# pa=os.listdir('C:\Users\HP\python\агрегатор\фото 1')
-# print(pa)
 
-for dirpath, dirs, files in os.walk(os.path.join(tek_dir)):
-    for file in files:
-        shutil.copy(dirpath+"\\"+file, dir2, follow_symlinks=True)  # каждый абсолютный путь файла переносим в папку фото_2
-# set_photo = set(files)
-    
+def aggregator(path_phone: str, path_PC: str):
+    dir_phone = os.listdir(path_phone) 
+    for file in dir_phone:
+        path_to_file = os.path.join(path_phone,file)                 
+        if os.path.isfile(path_to_file):                             # проверяем является ли файлом
+            shutil.copy(path_to_file, path_PC, follow_symlinks=True) # копируем на ПК
+        else:
+            new_path_PC = os.path.join(path_PC,file)                 # обновляем путь с учётом вложенности папок
+            os.mkdir(new_path_PC)                                    # создаем папку на ПК
+            new_path_phone = os.path.join(path_phone,file)           # новый путь для новой папки
+            aggregator(new_path_phone, new_path_PC)                  # рекурсия с новыми путями
+
 time.sleep(5)
-
 print(datetime.now() - start_time)
-# for _, _, files in os.walk(os.path.join(dir2)):
-#     set_photo_2 = set(dir2)
-# print(set_photo)
-# print(set_photo_2)
+
+def main():
+    aggregator(path_PC=PATH_PC, path_phone=PATH_PHONE)
+
+    
+if __name__ == '__main__':
+    main()
